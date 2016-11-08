eaced0fab364249792cdb2fad726bd548ee9cf92
meo84 <oopsiday@gmail.com>
2016-11-08 22:44:03 +0100

    linked what back cell to movie data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
 	def home
+		@movies_podium = [ Movie.rank_by_attendance(1), Movie.rank_by_attendance(2), Movie.rank_by_attendance(3)]
+		@movie1 = Movie.rank_by_attendance(1)
+		@movie2 = Movie.rank_by_attendance(2)
+		@movie3 = Movie.rank_by_attendance(3)
 	end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
 	has_many :events, as: :activity
+
+	def self.rank_by_attendance(rank)
+		ranks = Movie.all.sort_by { |movie| movie.events.last.attendees_nb }.reverse!
+		return ranks[rank-1]
+	end
 end
