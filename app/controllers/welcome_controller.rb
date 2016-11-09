e215a5e2c76e92a13d51401d3c885af88e06854a
meo84 <oopsiday@gmail.com>
2016-11-09 20:17:37 +0100

    included when back cell title with dynamic movie time data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,8 @@ class WelcomeController < ApplicationController
 
 	def home
 		@movies_podium = [ Movie.rank_by_attendance(1), Movie.rank_by_attendance(2), Movie.rank_by_attendance(3)]
-		@movie1 = Movie.rank_by_attendance(1)
-		@movie2 = Movie.rank_by_attendance(2)
-		@movie3 = Movie.rank_by_attendance(3)
+		@oldest_movie = Movie.oldest
+		@latest_movie = Movie.latest
 	end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,11 @@ class Movie < ActiveRecord::Base
 		ranks = Movie.all.sort_by { |movie| movie.events.last.attendees_nb }.reverse!
 		return ranks[rank-1]
 	end
+
+	scope :order_by_year, -> { all.order("year") }
+
+	scope :oldest, -> { order_by_year.first }
+
+	scope :latest, -> { order_by_year.last }
+
 end
