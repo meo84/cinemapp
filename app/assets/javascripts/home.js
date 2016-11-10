98a0ca1863ca856d6def79bc371134f9ac3db20c
meo84 <oopsiday@gmail.com>
2016-11-10 14:25:15 +0100

    attempt to toggle the what cell

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,10 @@
+	$('#what').toggle(function() {
+	  //hide what-front
+	  $('.what').hide();
+	  //display what-back
+	 	alert("test");
+	}, function() {
+	  //display what-front & hide what-back
+	  $('.what').show();
+	  alert("test gain");
+	});
