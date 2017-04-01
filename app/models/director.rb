ec4406b38ac8f3d6a686662d1e59a2462857d296
meo84 <oopsiday@gmail.com>
2017-04-01 14:23:38 +0200

    Test director associations + refacto
    - Use shoulda gem to refacto director validation tests
    - Use shoulda gem to implement director association tests

--- app/models/director.rb
+++ app/models/director.rb
@@ -2,7 +2,6 @@ class Director < ActiveRecord::Base
   validates :first_name, :last_name, :country, presence: true
   validates_format_of :country, with: /\A[A-Z]{2}\z/
 
-  has_many :movies
   has_many :directors_movies
   has_many :movies, through: :directors_movies
 
--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -1,30 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Director, type: :model do
-  subject { Director.new first_name: 'John', last_name: 'Doe', country: 'AU' }
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
+  describe "Validations" do
+
+    it "is not valid without a first name" do
+      should validate_presence_of(:first_name)
+    end
+
+    it "is not valid without a last name" do
+      should validate_presence_of(:last_name)
+    end
+
+    it "is not valid without a country" do
+      should validate_presence_of(:country)
+    end
+
+    it "is not valid without a 2-uppercase-letters country" do
+      should_not allow_value("some c0untry").for(:country)
+      should allow_value("AU").for(:country)
+    end
   end
 
-  it "is not valid without a first name" do
-    subject.first_name = nil
-    expect(subject).to_not be_valid
+  describe "Associations" do
+    it { should have_many(:movies).through(:directors_movies) }
+    it { should have_many(:directors_movies) }
   end
 
-  it "is not valid without a last name" do
-    subject.last_name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a country" do
-    subject.country = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a 2-uppercase-letters country" do
-    subject.country = "some c0untry"
-    expect(subject).to_not be_valid
-  end
 end
 
