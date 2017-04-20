0ac0070258accf833a9b57b4eaab1d1ae467840b
meo84 <oopsiday@gmail.com>
2017-04-21 01:20:00 +0200

    setup get ajax call on new movie form

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,19 +1,14 @@
 $(function() {
-  $('html').on('change', 'select#movie_event_ids.chosen', function(){
-    var form = $(this).parent('form');
+  $('html').on('change', '.new_movie select#movie_event_ids.chosen', function(){
 
-    $.ajax({
-      type: "GET",
-      url: form.attr('action') + "/new",
-      success: function(data){
-        var chosenEventID = $('#movie_event_ids').val();
-        var chosenEvent = data.find(function(event){ return event["id"] == chosenEventID; });
-        var chosenEventImageURL = chosenEvent["first_image_url"];
-        $('#movie_poster_url').val(chosenEventImageURL);
-      },
-      error: function(){
-        console.log('error during ajax call fetching poster url from event');
-      }
+    $.get("/movies/new", function(data) {
+      var chosenEventID = $('#movie_event_ids').val();
+      var chosenEvent = data.find(function(event){ return event["id"] == chosenEventID; });
+      var chosenEventImageURL = chosenEvent["first_image_url"];
+      $('#movie_poster_url').val(chosenEventImageURL);
+    })
+    .error(function() {
+      console.log("there was an error fetching the movie poster URL");
     });
   });
 });
