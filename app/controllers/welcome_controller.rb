76b55dd8c5680e5ce4f0cbde9575ca2c4673e6a3
meo84 <oopsiday@gmail.com>
2017-04-25 18:19:27 +0200

    Cleanup
    - remove user unnecessary controller and view
    - fix welcome controller indentations

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
 
-	def home
-		@movies_podium = [ Movie.rank_by_attendance(1), Movie.rank_by_attendance(2), Movie.rank_by_attendance(3)]
-		@oldest_movie = Movie.oldest
-		@latest_movie = Movie.latest
+  def home
+    @movies_podium = [ Movie.rank_by_attendance(1), Movie.rank_by_attendance(2), Movie.rank_by_attendance(3)]
+    @oldest_movie = Movie.oldest
+    @latest_movie = Movie.latest
 
-		@directors_podium = [ Director.rank_by_movie_nb(1), Director.rank_by_movie_nb(2), Director.rank_by_movie_nb(3) ]
-	end
+    @directors_podium = [ Director.rank_by_movie_nb(1), Director.rank_by_movie_nb(2), Director.rank_by_movie_nb(3) ]
+  end
 
 end
