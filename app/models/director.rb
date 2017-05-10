4a0b8073b677553737ba6d8239159a8a434df979
meo84 <oopsiday@gmail.com>
2017-05-10 16:40:52 +0200

    aerate code and fix indentations

--- app/models/director.rb
+++ app/models/director.rb
@@ -20,4 +20,5 @@ class Director < ActiveRecord::Base
   def fullname
     first_name + ' ' + last_name
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 8 }
+
 end
